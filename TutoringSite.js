91b2a15a2dce841bc019cbea980f80eba332a0a7
Sohail Mohammed <sohailm498@gmail.com>
2016-05-21 15:50:27 -0400

    updated the background and nav pannel

--- TutoringSite.js
+++ TutoringSite.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
   $(".navLists").click(function(){
 
-    $(".navLists").css({"border-bottom": "none", "background-color": "#efefef", "font-size": "100%", "color": "grey" });
-    $(this).css({"border-bottom": "2px solid black", "background-color": "lightblue", "font-size": "110%", "color": "black"});
+    $(".navLists").css({"border-bottom": "none", "background-color": "#efefef", "font-size": "1" });
+    $(this).css({"border-bottom": "2px solid black", "background-color": "lightblue", "font-size": "110%"});
   });
 
 });
