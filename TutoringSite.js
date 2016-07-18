7208e88dc1c9086bdbf16964da08f6eabf18ead1
Sohail Mohammed <sohailm498@gmail.com>
2016-07-18 14:39:58 -0400

    fixed toggle button

--- TutoringSite.js
+++ TutoringSite.js
@@ -3,6 +3,16 @@ $(document).ready(function(){
 
     $(".navLists").css({"border-bottom": "none", "background-color": "#efefef", "font-size": "1" });
     $(this).css({"border-bottom": "2px solid black", "background-color": "lightblue", "font-size": "110%"});
+    alert(this);
+  });
+
+  $(document).click(function(event){
+      var clickover = $(event.target);
+      var $navbar = $(".navbar-collapse");
+      var _opened = $navbar.hasClass("in");
+      if(_opened == true && !clickover.hasClass("navbar-toggle")){
+        $navbar.collapse("hide");
+      }
   });
 
 });
